2f508d989d5f98a1e9b25495e619d28f688a9666
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-02-25 19:30:59 +0530

    Delete validation folder

--- validation/userValidation.js
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-// This defines the user schema and the necessary and required fields
-// for the form
-export const userSchema = yup.object({
-  firstName: yup.string().required(),
-  lastName: yup.string().required(),
-  email: yup.string().email().required(),
-});
