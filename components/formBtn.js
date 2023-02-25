737d64e642fb47333050fb0e242bab701ddf2b0b
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2023-02-25 19:29:29 +0530

    Add another prop type for the button type

--- components/formBtn.js
+++ components/formBtn.js
@@ -1,6 +1,7 @@
-function FormButton({ btnName, handleClick }) {
+function FormButton({ btnName, type, handleClick }) {
   return (
     <button
+      type={type}
       onClick={handleClick}
       className="bg-emerald-500 text-white rounded-full px-4 py-1 sm:py-2 my-3 w-40"
     >
